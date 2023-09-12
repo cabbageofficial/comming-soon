99dcbf08aadff8040c6d87a4e2e0e960cf58046d
Siam Ahnaf <siamahnaf198@gmail.com>
2023-09-13 00:36:56 +0600

    Adding Staging Info

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,13 @@ import Icon from "./component/icon";
 const Page = () => {
   return (
     <div className="w-full h-screen flex justify-center items-center">
-      <div>
+      <div className="text-center">
         <Image src="/logo.png" width={400} height={400} alt="cabbage" className="w-[100px] mx-auto" />
         <h4 className="text-2xl font-bold mb-2">Coming <span className="text-secondary">Soon</span></h4>
-        <div className="flex gap-2 items-center">
+        <h6 className="font-medium mb-4">Our site is under maintenance</h6>
+        <Link href="https://staging.cabbageweb.com/" className="bg-secondary text-white py-1.5 px-4 uppercase text-sm font-semibold rounded">Visit</Link>
+        <p className="mt-4 text-sm uppercase">Or</p>
+        <div className="flex gap-2 items-center justify-center -ml-2">
           <Icon />
           <p className="font-medium">
             <Link href="https://wa.me/message/UAXIYNES562EN1">
